3db3608ae2cc770e2d0e787229d8cc67b5700b8f
Alexander Feiglstorfer <delooks@gmail.com>
2018-09-21 07:41:40 +0200

    Fixes client navigation not triggering content change

--- src/templates/storyblok-entry.js
+++ src/templates/storyblok-entry.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 class StoryblokEntry extends React.Component {
+  static getDerivedStateFromProps(props, state) {
+    if (state.story.uuid === props.pageContext.story.uuid) {
+      return null
+    }
+
+    return StoryblokEntry.prepareStory(props)
+  }
+
+  static prepareStory(props) {
+    const story = Object.assign({}, props.pageContext.story)
+    story.content = JSON.parse(story.content)
+    
+    return { story }
+  }
+
   constructor(props) {
     super(props)
-    let story = Object.assign({}, props.pageContext.story)
-    story.content = JSON.parse(story.content)
-    this.state = {story: story}
+
+    this.state = StoryblokEntry.prepareStory(props)
   }
 
   render() {
